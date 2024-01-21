fafbd0c1cf88a8f98b1d0df9047e093d300495e6
Tim <timsopost@web.de>
2024-01-21 07:06:15 +0100

    lr adjusted

--- src/pseudowords/get_kee_pseudowords_avg.py
+++ src/pseudowords/get_kee_pseudowords_avg.py
@@ -218,7 +218,7 @@ class Coercion:
 
     def _train(self, model, vec_targets, queries, targets1):
         loss_fct = nn.MSELoss(reduction='mean')  # mean will be computed later
-        optimizer = torch.optim.AdamW(model.parameters(), lr=0.3)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=0.00035)
         epoch = 5000 // len(queries)  # 1000 == 5000//5 was the default for BERT
         scheduler = get_linear_schedule_with_warmup(
             optimizer,
